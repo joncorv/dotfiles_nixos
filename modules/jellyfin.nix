6411a6b9ce35a93e9fe4790bf5a2f5462df5d4aa
joncorv <joncorv@gmail.com>
2025-12-05 09:48:31 -0800

    jellyfin added to andromeda

--- modules/jellyfin.nix
+++ modules/jellyfin.nix
@@ -12,5 +12,33 @@
     pkgs.jellyfin-web
     pkgs.jellyfin-ffmpeg
   ];
+  # Add jellyfin user to render group for GPU access
+  users.users.jellyfin.extraGroups = [
+    "render"
+    "video"
+    "media"
+  ];
+
+  # Enable hardware acceleration packages
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # For Intel iGPU (if you have one)
+      intel-vaapi-driver # Fallback for older codecs
+      libvdpau-va-gl
+      libva-vdpau-driver
+    ];
+  };
+  systemd.tmpfiles.rules = [
+    "d /srv/media 0775 astro media -"
+    "d /srv/media/videos 0775 astro media -"
+  ];
+
+  users.groups.media = {
+    members = [
+      "astro"
+      "jellyfin"
+    ];
+  };
 
 }
